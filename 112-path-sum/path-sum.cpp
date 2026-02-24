6d5f3f251cecfc62cdb3700aeb9624cf44256093
Paarth Bhatnagar <143537794+paarth-9k@users.noreply.github.com>
2026-02-24 23:03:24 +0530

    Time: 3 ms (3.79%) | Memory: 21.6 MB (33.72%) - LeetSync

---
+++ 112-path-sum/path-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool dfs(TreeNode* root, int target) {
+        if (!root)
+            return false;
+
+        target -= root->val;
+
+        // If leaf node
+        if (!root->left && !root->right) {
+            return target == 0;
+        }
+
+        return dfs(root->left, target) ||
+               dfs(root->right, target);
+    }
+
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return dfs(root, targetSum);
+    }
+};
