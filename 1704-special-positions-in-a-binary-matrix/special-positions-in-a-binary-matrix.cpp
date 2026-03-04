db7cf50d713e34b6d2885e75c15731c428a9ae77
Paarth Bhatnagar <143537794+paarth-9k@users.noreply.github.com>
2026-03-04 19:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (20.57%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int ct = 0;
         int m = mat.size();
         int n = mat[0].size();
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(mat[i][j] == 1)
-                {
-                    int rones = 0;
-                    for(int k = 0; k < m; k++)
-                    {
-                        if(mat[k][j] == 1)
-                            rones++; 
-                    }
-                    int cones = 0;
-                    for(int k = 0; k < n; k++)
-                    {
-                        if(mat[i][k] == 1)
-                            cones++; 
-                    }
-                    if(rones == 1 && cones == 1)
-                    {
-                        ct++;
-                    }
+        vector<int> row(m, 0);
+        vector<int> column(n, 0);
+
+            for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    row[i]++;
+                    column[j]++;
                 }
             }
         }
+
+        int ct = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && column[j] == 1) {
+                    ct++;
+                }
+            }
+        }
+
         return ct;
     }
 };
